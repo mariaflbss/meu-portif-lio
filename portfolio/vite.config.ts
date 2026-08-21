9274ab8fb48d34afd95d1e1f566b4b6764d05a69
Maria Fernanda Laboissiere <mariaf.laboissiere@gmail.com>
2026-08-21 17:57:12 -0300

    fix: corrigindo base do Vite para Vercel

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -4,8 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/meu-portifolio-/",
-
   plugins: [
     react(),
     tailwindcss(),
